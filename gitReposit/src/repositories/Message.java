f9b8d28dc3707da55a849409da01e0a9546d7ae3
YaseenYahiya <yaseenyahiya6@gmail.com>
2016-07-17 19:08:53 -0500

    Fetch test02

--- gitReposit/src/repositories/Message.java
+++ gitReposit/src/repositories/Message.java
@@ -8,6 +8,7 @@ public class Message {
 	public static void main(String[] args) {
 System.out.println("code repositing ");
 System.out.println("New comment test");
+System.out.println("Fetch test");
 	}
 
 }
